f2f969eb992eb572c0816e395d376cf2da5c9f7d
Sreenivasulu57 <vsreenivasulu241@gmail.com>
2025-09-22 10:29:20 +0530

    User Post

---
+++ VSC.Toolsy.Common/DTOs/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSC.Toolsy.Common.DTOs
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(T data, string message = "Success")
+            => new() { Success = true, Data = data, Message = message };
+
+        public static ApiResponse<T> FailureResponse(string message)
+            => new() { Success = false, Message = message };
+    }
+}
--- VSC.Toolsy.Services/UserService.cs
+++ VSC.Toolsy.Services/UserService.cs
@@ -1,26 +1,40 @@
-﻿using VSC.Toolsy.Common.Interfaces;
+﻿using VSC.Toolsy.Common.DTOs.Requests;
+using VSC.Toolsy.Common.Interfaces;
 using VSC.Toolsy.Common.Models.CoreEntites;
 using VSC.Toolsy.Repositories.Interfaces;
-
+using BCrypt.Net;
 namespace VSC.Toolsy.Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
 
+
         public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
         }
 
-        public async Task<User> GetByIdAsync(int id)
+        public async Task<User> SaveAsync(RegisterUserDto registerUserDto)
         {
-            if (id <= 0)
+            User user = new
+       User
             {
-                throw new ArgumentException($"Id {id} Must Be Greater Than Zero");
-            }
 
-            return await _userRepository.GetByIdAsync(id);
+                FirstName = registerUserDto.FirstName,
+                LastName = registerUserDto.LastName,
+                Email = registerUserDto.Email,
+                PhoneNumber = registerUserDto.PhoneNumber,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(registerUserDto.Password),
+                ProfileImageUrl = registerUserDto.ProfileImageUrl,
+                CreatedBy = registerUserDto.Email,
+            };
+
+            await _userRepository.SaveAsync(user);
+
+            await _userRepository.SaveChangesAsync();
+
+            return user;
         }
     }
 }
