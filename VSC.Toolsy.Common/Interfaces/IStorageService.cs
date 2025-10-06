8748e0e532713d4d996ed9cb68d9c69102bd7fb8
yaswanth143411 <yaswanthkumarbogineni@gmail.com>
2025-10-06 18:12:41 +0530

    added localstorage to save files

---
+++ VSC.Toolsy.Common/Interfaces/IStorageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSC.Toolsy.Common.Interfaces
+{
+    public interface IStorageService
+    {
+        string GetMediaUrl(string fileName);
+
+        Task<string> SaveMediaAsync(Stream mediaBinaryStream, string fileName, string mimeType = null);
+
+        Task<bool> DeleteMediaAsync(string fileName);
+    }
+}
